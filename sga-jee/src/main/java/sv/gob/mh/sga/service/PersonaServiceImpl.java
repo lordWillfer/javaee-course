b27440ccaf86d022e58ce1418ade92087c89007f
Williams E. Fernández <williamsenrique@gmail.com>
2015-06-23 14:35:49 -0600

    Agregando configuraciones de la capa de datos DAO para la clase Persona - Mapeo de la clase entidad Persona con JPA estandar contro los campos de la base de datos - Creación de las clase EJB PersonaDao y PersonaDAOImpl que incluye los métodos de acceso a la base de datos - Inyección de la clase DAO en la capa de servicios PersonaServiceImpl

--- sga-jee/src/main/java/sv/gob/mh/sga/service/PersonaServiceImpl.java
+++ sga-jee/src/main/java/sv/gob/mh/sga/service/PersonaServiceImpl.java
@@ -1,44 +1,40 @@
 package sv.gob.mh.sga.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 import sv.gob.mh.sga.domain.Persona;
+import sv.gob.mh.sga.eis.PersonaDao;
 
 @Stateless
 public class PersonaServiceImpl implements PersonaServiceRemote, PersonaService {
 
+	@EJB
+	private PersonaDao personaDao;
+	
 	public List<Persona> listarPersonas() {
-		List<Persona> personas = new ArrayList<Persona>();
-		personas.add(new Persona(1, "Juan", "Perez", "Suarez", "jperez@gmail.com", "55668798"));
-		personas.add(new Persona(2, "Williams", "Fernández", "Rodríguez", "williamsenrique@gmail.com", "78605555"));
-		return personas;
+		return personaDao.findAllPersonas();
 	}
 
 	public Persona encontrarPersonaPorId(Persona persona) {
-		// TODO Auto-generated method stub
-		return null;
+		return personaDao.findPersonaById(persona);
 	}
 
 	public Persona encontrarPerosnaPorEmail(Persona persona) {
-		// TODO Auto-generated method stub
-		return null;
+		return personaDao.findPersonaByEmail(persona);
 	}
 
 	public void registrarPersona(Persona persona) {
-		// TODO Auto-generated method stub
-
+		personaDao.insertPersona(persona);
 	}
 
 	public void modificarPersona(Persona persona) {
-		// TODO Auto-generated method stub
-
+		personaDao.updatePersona(persona);
 	}
 
 	public void eliminatPersona(Persona persona) {
-		// TODO Auto-generated method stub
-
+		personaDao.deletePersona(persona);
 	}
 }
